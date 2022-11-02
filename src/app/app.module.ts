8a01cfd0df036178dfe80869f7ff5508ddf516d2
Nicolas Colque <nicolascon93@gmail.com>
2022-11-02 10:34:46 -0400

    Creacion de interface, servicio, y la vista de busqueda Por Pais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
@@ -7,7 +9,7 @@ import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, PaisModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, PaisModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,16 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class PorPaisComponent implements OnInit {
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
 
-  constructor() { }
+  constructor(private paisSvc: PaisService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  buscar() {
+    this.hayError = false;
+    console.log(this.termino);
+
+    this.paisSvc.buscarPais(this.termino).subscribe(
+      (paises) => {
+        console.log(paises);
+        this.paises = paises;
+      },
+      (err) => {
+        this.hayError = true;
+        this.paises = [];
+      }
+    );
   }
-
 }
