625c3816aef14ae4b8b3be0e1356826954da76ff
Nicolas Colque <nicolascon93@gmail.com>
2023-04-12 14:34:54 -0400

    Creacion de la vista VER MAS o DETALLE de un Pais

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,16 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/pais';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class VerPaisComponent implements OnInit {
+  pais!: Country[];
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private paisSvc: PaisService
+  ) {}
 
   ngOnInit(): void {
-  }
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }) => this.paisSvc.getPaisPorAlpha(id)),
+        tap(console.log)
+      )
+      .subscribe((pais) => {
+        this.pais = pais;
+      });
 
+    /* this.activatedRoute.params
+    .subscribe(({id}) => {
+      console.log(id);
+
+      this.paisSvc.getPaisPorAlpha(id)
+      .subscribe(pais => {
+        console.log(pais);
+      });
+    }); */
+  }
 }
